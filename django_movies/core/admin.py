0492b22013b3ee4a6dd93a3e3cacd37b784accd4
Michał Kałużny <zych91@gmail.com>
2020-09-06 12:14:36 +0200

    add new models

--- django_movies/core/admin.py
+++ django_movies/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.urls import path
+from core.models import Movie, Genre, Director
+from core.views import hello
+
+admin.site.register(Movie)
+
+admin.site.register(Genre)
+
+admin.site.register(Director)
 
 # Register your models here.
--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -3,12 +3,42 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    general_audiences = "G"
+    parental_guidance_suggested = "PG"
+    parents_strongly_cautioned = "PG-13"
+    restricted = "R"
+    adults_only = "NC-17"
+    age_limit_choices = [
+        (general_audiences, "G"),
+        (parental_guidance_suggested, "PG"),
+        (parents_strongly_cautioned, "PG-13"),
+        (restricted, "R"),
+        (adults_only, "NC-17")
+    ]
+    age_limit=models.CharField(max_length=5, null=True, blank=True, choices=age_limit_choices, default=general_audiences)
+
+    def __str__(self):
+        return self.name
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=120)
     rating = models.IntegerField(null=True, validators=[MaxValueValidator(10), MinValueValidator(1)])
     released = models.DateField()
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
+    director = models.ForeignKey(Director, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.title} from {self.released}"
+
