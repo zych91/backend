80b16709ed83aac0f992ddbf3ac52b6c2b9825cb
Michał Kałużny <zych91@gmail.com>
2020-09-27 10:12:27 +0200

    register profile model

--- django_movies/core/admin.py
+++ django_movies/core/admin.py
@@ -3,6 +3,8 @@ from django.urls import path
 from core.models import Movie, Genre, Director, Country
 from core.views import hello
 
+from accounts.models import Profile
+
 admin.site.register(Movie)
 
 admin.site.register(Genre)
@@ -10,5 +12,7 @@ admin.site.register(Genre)
 admin.site.register(Director)
 
 admin.site.register(Country)
+
+admin.site.register(Profile)
 
 # Register your models here.
