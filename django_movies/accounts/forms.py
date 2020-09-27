bcaaf2e647613f21f868279d96946652bcd6ef01
Michał Kałużny <zych91@gmail.com>
2020-09-27 10:09:49 +0200

    add shoe size

--- django_movies/accounts/forms.py
+++ django_movies/accounts/forms.py
@@ -1,7 +1,10 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UserCreationForm
-from django.forms import Form
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.forms import Form, IntegerField
+
+from accounts.models import Profile
 
 
 class SubmittableForm(Form):
@@ -18,7 +21,19 @@ class SubmittableAuthenticationForm(SubmittableForm, AuthenticationForm):
 class SubmittablePasswordChangeForm(SubmittableForm, PasswordChangeForm):
     pass
 
+
 class SignUpForm(SubmittableForm, UserCreationForm):
+    shoe_size = IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(50)]
+    )
 
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'first_name']
+
+    def save(self, commit=True, *args, **kwargs):
+        user = super().save(commit)
+        shoe_size = self.cleaned_data['shoe_size']
+        profile = Profile(shoe_size=shoe_size, user=user)
+        profile.save()
+        return user
+
